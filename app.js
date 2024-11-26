798bb92cc87bb90e816b8631a3443d9509da1cf3
OuaailElaouad <2003waelelaouad@gmail.com>
2024-11-26 04:50:59 +0100

    create reducer with Builder callback notation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { createAction, nanoid } = require("@reduxjs/toolkit");
+const { createAction, nanoid, createReducer } = require("@reduxjs/toolkit");
 //InitialisteState
 const initialisteState = {
     counter: 0,
@@ -12,11 +12,31 @@ const incrementBy = createAction("INCREMENT_BY", (amount, user) => {
     return {
         payload: {
             amount,
-            user,
-            id:nanoid(),
         },
     };
 });
-console.log(incrementBy('ouaail',20));
 //Reducer
+//Create Reducer
+//1.Builder callback
+//2.map object notaion
+
+//Builder callback
+createReducer(initialisteState, (builder) => {
+    //increment
+    builder.addCase(increment, (state) => {
+        state.counter += 1;
+    });
+    //decrement
+    builder.addCase(decrement, (state) => {
+        state.counter -= 1;
+    });
+    //reset
+    builder.addCase(resetCounter, (state) => {
+        state.counter = 0;
+    });
+    //unvrement By
+    builder.addCase(incrementBy, (state) => {
+        state.counter += isAction.payload.amount;
+    });
+})
 //Store
